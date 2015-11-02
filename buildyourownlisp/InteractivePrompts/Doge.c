6bb51901f0cdfe12569fc42b1973248a10668a55
Sanket K <hckrtst@gmail.com>
2015-11-02 08:04:14 -0800

    Add a simple Doge parser

--- buildyourownlisp/InteractivePrompts/Doge.c
+++ buildyourownlisp/InteractivePrompts/Doge.c
@@ -1,7 +1,10 @@
+// Build with -lm option
+//
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "mpc.h"
+#include "linenoise.h"
 
 #define INPUT_BUFFER_LEN 48
 // Declare this as static so as to avoid name collision
@@ -9,16 +12,40 @@
 static char input_buf[INPUT_BUFFER_LEN];
 
 int main(int argc, char** argv) {
-    mpc_parser_t* Adjective = mpc_or(4,
-            mpc_sym("wow"), mpc_sym("many"),
-            mpc_sym("so"), mpc_sym("such")
-    );
+    /* Create parsers */
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expression = mpc_new("expression");
+    mpc_parser_t* Lispy = mpc_new("lispy");
+    
+    linenoiseClearScreen();
 
-    mpc_parser_t* Nound = mpc_or(5,
-            mpc_sym("lisp"), mpc_sym("book"),
-            mpc_sym("hammer"), mpc_sym("laser"),
-            mpc_sym("rocket")
-    );
+    /* Define them with the language as follows */
+    mpca_lang(MPCA_LANG_DEFAULT, 
+            "\
+                number      : /-?[0-9]+/;\
+                operator    : '+' | '/' | '*' | '-';\
+                expression  : <number> | '(' <operator> <expression>+ ')';\
+                lispy        : /^/ <operator> <expression> /$/;\
+            ", Number, Operator, Expression, Lispy);
+    char *line = NULL;
+
+    while((line = linenoise("Lispy> ")) != NULL ) {
+        mpc_result_t result;
+
+        if (mpc_parse("<stdin>" , line, Lispy, &result)) {
+           mpc_ast_print(result.output);
+           mpc_ast_delete(result.output);
+        } else {
+            mpc_err_print(result.output);
+            mpc_err_delete(result.output);
+
+        }
+
+        free(line);
+    }
+
+    mpc_cleanup(4, Number, Operator, Expression, Lispy);
 
     return 0;
    
