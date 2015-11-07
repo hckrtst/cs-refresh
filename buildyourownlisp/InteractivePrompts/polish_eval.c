b567ca4b7e8d288a1e38434d2a5f4cafc77022ad
Sanket K <hckrtst@gmail.com>
2015-11-07 14:05:05 -0800

    checkpoint for eval example

---
+++ buildyourownlisp/InteractivePrompts/polish_eval.c
@@ -0,0 +1,66 @@
+// Build with -lm option
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mpc.h"
+#include "linenoise.h"
+
+#define INPUT_BUFFER_LEN 48
+// Declare this as static so as to avoid name collision
+// if we link this with another module
+static char input_buf[INPUT_BUFFER_LEN];
+
+long eval(mpc_ast_t *t) {
+    /* numbers can be returned directly */
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents)
+    }
+    
+    /* first child is '(' so second child is operator */
+    // TODO
+}
+
+int main(int argc, char** argv) {
+    /* Create parsers */
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* TextOp = mpc_new("textop");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expression = mpc_new("expression");
+    mpc_parser_t* Lispy = mpc_new("lispy");
+    
+    linenoiseClearScreen();
+
+    /* Define them with the language as follows */
+    mpca_lang(MPCA_LANG_DEFAULT, 
+            " number      : /-?[0-9]+/ ; "
+            " textop      : /(mult|div)/ ; "
+             " operator    : '+' | '/' | '*' | '-' | <textop> ; "
+             " expression  : <number> | '(' <operator> <expression>+ ')' ; "
+             " lispy        : /^/ <operator>* <expression> /$/ ; ", Number, TextOp, Operator, Expression, Lispy);
+    char *line = NULL;
+
+    while((line = linenoise("Lispy> ")) != NULL ) {
+        mpc_result_t result;
+
+        if (mpc_parse("<stdin>" , line, Lispy, &result)) {
+           mpc_ast_print(result.output);
+           mpc_ast_delete(result.output);
+        } else {
+            mpc_err_print(result.output);
+            mpc_err_delete(result.output);
+        }
+
+        free(line);
+    }
+
+    mpc_cleanup(5, Number, Operator, Expression, Lispy, TextOp);
+
+    return 0;
+   
+}
+
+/* 
+ * Try this expression:
+ * Lispy> (* ( - 6 7 ) 8 )
+*/
