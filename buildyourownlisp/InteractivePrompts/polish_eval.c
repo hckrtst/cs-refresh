1c180a431eec28ffa3a2cf83218a1a8d7fd8b9dd
Sanket K <hckrtst@gmail.com>
2015-11-08 20:30:31 -0800

    fix grammar to resolve segfault
    Also add some debugging logs

--- buildyourownlisp/InteractivePrompts/polish_eval.c
+++ buildyourownlisp/InteractivePrompts/polish_eval.c
@@ -25,28 +25,32 @@ long eval_op(long x, const char* operator, long y) {
     return 0;
 }
 
-long eval(mpc_ast_t *t) {
+
+long eval(mpc_ast_t *t, int tabsize) {
     /* numbers can be returned directly */
     if (strstr(t->tag, "number")) {
-        printf("test\n");
-        dbg("got number = %s\n", t->contents);
+        for(int i = 0; i <= tabsize; i++) dbg("\t%s", "");
+        dbg("operand = %s\n", t->contents);
         return atoi(t->contents);
     }
     
     /* first child is '(' so second child is operator */
     char *op = t->children[1]->contents;
+    
+    for(int i = 0; i <= tabsize; i++) dbg("\t%s", "");
+    dbg("operation = %s\n", t->children[1]->contents);
 
     /* evaluate the first operand and store as 
      * result 
      */
-    long result = eval(t->children[2]);
+    long result = eval(t->children[2], tabsize + 1);
 
     /* evaluate the rest of the operands and recursively
      * calls their children to get the final output
      */
     int index = 3;
     while (strstr(t->children[index]->tag, "expression")) {
-        result = eval_op(result, op, eval(t->children[index]));
+        result = eval_op(result, op, eval(t->children[index], tabsize + 1));
         index++;
     }
 
@@ -56,7 +60,6 @@ long eval(mpc_ast_t *t) {
 int main(int argc, char** argv) {
     /* Create parsers */
     mpc_parser_t* Number = mpc_new("number");
-    mpc_parser_t* TextOp = mpc_new("textop");
     mpc_parser_t* Operator = mpc_new("operator");
     mpc_parser_t* Expression = mpc_new("expression");
     mpc_parser_t* Lispy = mpc_new("lispy");
@@ -66,17 +69,16 @@ int main(int argc, char** argv) {
     /* Define them with the language as follows */
     mpca_lang(MPCA_LANG_DEFAULT, 
             " number      : /-?[0-9]+/ ; "
-            " textop      : /(mult|div)/ ; "
-             " operator    : '+' | '/' | '*' | '-' | <textop> ; "
+             " operator    : '+' | '/' | '*' | '-'; "
              " expression  : <number> | '(' <operator> <expression>+ ')' ; "
-             " lispy        : /^/ <operator>* <expression> /$/ ; ", Number, TextOp, Operator, Expression, Lispy);
+             " lispy        : /^/ <operator> <expression>+ /$/ ; ", Number, Operator, Expression, Lispy);
     char *line = NULL;
 
     while((line = linenoise("Lispy> ")) != NULL ) {
         mpc_result_t result;
 
         if (mpc_parse("<stdin>" , line, Lispy, &result)) {
-            long r = eval(result.output);
+            long r = eval(result.output, 0);
             printf("%li\n", r);
             mpc_ast_delete(result.output);
         } else {
@@ -87,7 +89,7 @@ int main(int argc, char** argv) {
         free(line);
     }
 
-    mpc_cleanup(5, Number, Operator, Expression, Lispy, TextOp);
+    mpc_cleanup(4, Number, Operator, Expression, Lispy);
 
     return 0;
    
