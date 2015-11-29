925b5a80462b29790d3385c88356d0bd3eafa1c2
Sanket K <hckrtst@gmail.com>
2015-11-29 10:28:11 -0800

    exit gracefully on ESC

--- buildyourownlisp/InteractivePrompts/ncurses2.c
+++ buildyourownlisp/InteractivePrompts/ncurses2.c
@@ -5,26 +5,48 @@
 #include <math.h>
 
 #define MATH_PI 3.1418
-
+#define TIMEOUT 250
 
 static struct timespec ts = {.tv_sec = 0, .tv_nsec = 100000000};
 
+static void check_user_input() {
+    int ch = getch();
+    if (ch == 27) {
+            wclear(stdscr);
+            int row, col;
+            getmaxyx(stdscr, row, col);
+            attron(A_REVERSE);
+            mvprintw(row/2, col/2, "Bye now!");
+            refresh();
+            sleep(3);
+            endwin();
+            exit(0);
+        } else{
+            // include case that ch == ERR for no input
+            // do nothing
+        }
+
+}
+
 void draw_shape() {
-    int delta = 0.1;
     for (float rad = 0.0f; rad < 25;  rad=rad+0.1) {
-        delta+=1;
         mvaddch(25 + 11*sin(2*rad), 75 + 48*cos(rad), 'o');
         refresh();
         mvprintw(50,50,"Radians : %2.3f", rad); 
         refresh();
+        check_user_input();
         nanosleep(&ts, NULL);
     }
 }
+
+
 
 int main(int argc, char **argv) {
     // basic setup
     initscr();
     raw();
+    timeout(TIMEOUT);
+    noecho();
 
     // seed random
     srand(time(NULL));
@@ -40,9 +62,13 @@ int main(int argc, char **argv) {
         float y = rand() % col;
         mvprintw(x, y, "meow! at (%2.2f ,%2.2f)", x, y);
         refresh();
-            
+      
+        
+        check_user_input();
+        
         if (nanosleep(&ts, NULL) < 0) {
             printw("Adios muchachos!");
+            endwin();
             break;
         }
         if (max < 0) break;
