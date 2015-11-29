63fbdf31e7a24aa8d92539b8a5837bb136fa224b
Sanket K <hckrtst@gmail.com>
2015-11-29 09:38:40 -0800

    draw infinity shape

--- buildyourownlisp/InteractivePrompts/ncurses2.c
+++ buildyourownlisp/InteractivePrompts/ncurses2.c
@@ -7,13 +7,13 @@
 #define MATH_PI 3.1418
 
 
-static struct timespec ts = {.tv_sec = 0, .tv_nsec = 400000000};
+static struct timespec ts = {.tv_sec = 0, .tv_nsec = 100000000};
 
-void draw_circle(int ox, int oy, int r) {
+void draw_shape() {
     int delta = 0.1;
-    for (float rad = 0.0f; rad < 32;  rad=rad+0.4) {
+    for (float rad = 0.0f; rad < 25;  rad=rad+0.1) {
         delta+=1;
-        mvaddch(delta, 10 + 14*sin(rad/2), 'x');
+        mvaddch(25 + 11*sin(2*rad), 75 + 48*cos(rad), 'o');
         refresh();
         mvprintw(50,50,"Radians : %2.3f", rad); 
         refresh();
@@ -34,7 +34,7 @@ int main(int argc, char **argv) {
     getmaxyx(stdscr, row, col);
     
 
-    /*while(1) {
+    while(1) {
         max--;
         float x = rand() % row;
         float y = rand() % col;
@@ -46,10 +46,11 @@ int main(int argc, char **argv) {
             break;
         }
         if (max < 0) break;
-    }*/
+    }
 
-    draw_circle(100, 100, 30);
+    draw_shape();
 
+    sleep(3);
     endwin();
     return 0;
 }
