0f6a8f76beda5c63a87de8be0aaecff9908e4380
Sanket K <hckrtst@gmail.com>
2016-05-14 16:19:24 -0700

    p13

--- problems/P13/p13.c
+++ problems/P13/p13.c
@@ -1,2 +1,12 @@
 #include<stdio.h>
+#include<assert.h>
 
+int getbits(unsigned x, unsigned int p, unsigned int n) {
+   return ((x >> (p - n +1)) & ~(~0 << n));
+}
+int main(int argc, char * argv[]) {
+   unsigned x = 0b10001101101; 
+   printf("Input = %p, output = %p\n", x, getbits(x, 5, 4));
+
+   return 0;
+}
