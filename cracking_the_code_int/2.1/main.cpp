93d4c63dd0ab3a44d132ac587f3dd273a7576ffd
Sanket K <hckrtst@gmail.com>
2016-06-24 21:36:14 -0700

    linked list in the works

--- cracking_the_code_int/2.1/main.cpp
+++ cracking_the_code_int/2.1/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
-using namespace std;
+
+namespace sk {
+
+class List {
+   
+   class Node {
+      Node* next;
+      int data;
+      Node() {}
+   public:
+      Node(int data):data(data) {
+      }
+      // Supress the copy ctor
+      // Bad joojoo if we shallow copy a 
+      // node
+      Node(const Node& that) = delete; 
+            
+   };
+};
+
+};
 
 int main() {
+   
+   sk::List list;
    return 0;
 } 
