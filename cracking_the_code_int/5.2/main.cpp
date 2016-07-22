898e5e6ba25f58bc015de3621977e37ca3492227
Sanket K <hckrtst@gmail.com>
2016-07-22 07:44:55 -0700

    fix bug for 0 case

--- cracking_the_code_int/5.2/main.cpp
+++ cracking_the_code_int/5.2/main.cpp
@@ -7,12 +7,15 @@ string dec2bin(int n) {
     if (n > 1) {
         int rem = n % 2;
         return (dec2bin(n/2) + to_string(rem));
-    } else {
+    } else if (n == 1){
         return "1";
+    } else {
+      return "0";
     }
 }
 
 int main() {
     cout << dec2bin(22) << endl;
+    cout << dec2bin(0) << endl;
     return 0;
-} 
+}
