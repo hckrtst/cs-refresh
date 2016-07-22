11c15454a24c0f824cb357257d4c3c5c48ca0f56
Sanket K <hckrtst@gmail.com>
2016-07-22 08:27:50 -0700

    5.2 working

--- cracking_the_code_int/5.2/main.cpp
+++ cracking_the_code_int/5.2/main.cpp
@@ -14,8 +14,49 @@ string dec2bin(int n) {
     }
 }
 
+void extract(const string src, string& left, string& right) {
+   int pos = src.find(".");
+   if (pos == string::npos) {
+      // no decimal point
+      left = src;
+      right = "0";
+   } else {
+      left = src.substr(0, pos);
+      right = src.substr(pos+1, src.size());
+   }
+}
+
+string compose(const string left, const string right) {
+   string s;;
+   s.append(left);
+   s.append(".");
+   s.append(right);
+   return s;
+}
+
+bool is_valid(const string src) {
+   for (int i=0; i < src.size(); i++) {
+      if (!((isdigit(src[i])) || src[i] == '.'))
+         return false;
+   }
+   return true;
+}
+
+void solve(string src) {
+   if (!is_valid(src)) {
+      cout << src << " = ERROR" << endl;
+   } else {
+      string left, right;
+      extract(src, left, right);
+
+      cout << src << " = " << compose(dec2bin(atoi(left.c_str())),
+         dec2bin(atoi(right.c_str()))) << endl;
+   }
+}
+
 int main() {
-    cout << dec2bin(22) << endl;
-    cout << dec2bin(0) << endl;
+    solve("32");
+    solve("0000012.345");
+    solve("hello");
     return 0;
 }
