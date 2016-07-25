9b029f8d021a01e969110b05af53ab56af611741
hckrtst <hckrtst@gmail.com>
2016-07-24 21:27:04 -0700

    hobbling along on 8.3

--- cracking_the_code_int/8.3/main.cpp
+++ cracking_the_code_int/8.3/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <set>
+#include <algorithm>
+
 using namespace std;
+using subset_t = set<int>;
+using supaset_t = set<subset_t>;
+set<int> supaset;
 
 int main() {
+   // given set
+   set<int> input;
+   input.insert(2);
+   input.insert(3);
+   input.insert(4);
+   input.insert(5);
+   for (auto e: input) {
+      cout << e << ",";
+   }
+   cout << endl; 
+   
+   supaset_t supa;
+   for (auto e: input) {
+      subset_t s;
+      s.insert(e);
+      supa.insert(s);
+   }
+   
+   
    return 0;
-} 
+}
+
