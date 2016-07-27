42ed17aa0c9a287551446b4f66a218b8035272e3
hckrtst <hckrtst@gmail.com>
2016-07-26 20:11:55 -0700

    finding subsets

--- cracking_the_code_int/8.3/Readme.md
+++ cracking_the_code_int/8.3/Readme.md
@@ -2,6 +2,15 @@
 Write a method that returns all subsets of a set.
 
 #Notes
+(1,2,3,4)
+
+(1)
+(2)
+(3)
+(4)
+(1,2)
+(1,3)
+(1,4)
 
 ##Constraints and Assumptions
 
--- cracking_the_code_int/8.3/main.cpp
+++ cracking_the_code_int/8.3/main.cpp
@@ -5,7 +5,45 @@
 using namespace std;
 using subset_t = set<int>;
 using supaset_t = set<subset_t>;
-set<int> supaset;
+
+static void pr_set(subset_t s) {
+   for (auto e: s) {
+      cout << e << ",";
+   }
+   cout << endl;
+}
+
+supaset_t getSubSet(subset_t s) {
+   
+   if (s.empty()) {
+      cout << "empty" << endl;
+      return supaset_t();
+   }
+
+   if (s.size() == 1) {
+      cout << "elem" << *(s.begin()) << endl;
+      supaset_t su;
+      su.insert(s);
+      return su;
+   }
+   
+   int a = *(s.begin());
+   s.erase(a);
+   supaset_t sup = getSubSet(s);
+   for (supaset_t::iterator it = sup.begin(); it != sup.end(); it++) {
+      subset_t sub = *it;
+      sub.insert(a);
+      sup.insert(sub);
+   }
+   subset_t sub;
+   sub.insert(a);
+   sup.insert(sub);
+   for (auto e: sup) {
+      pr_set(e);
+   }
+   cout << endl;
+   return sup;
+}
 
 int main() {
    // given set
@@ -19,13 +57,7 @@ int main() {
    }
    cout << endl; 
    
-   supaset_t supa;
-   for (auto e: input) {
-      subset_t s;
-      s.insert(e);
-      supa.insert(s);
-   }
-   
+   getSubSet(input);    
    
    return 0;
 }
